72074a1e37276878decab8137260ffe07dff067f
Valdo <vteixeiraromao@gmail.com>
2023-07-27 16:56:48 +0100

    Removed Gentleman after stairs on departure gate

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,20 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+let popup: any = undefined;
+let website: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
 
     WA.room.onEnterLayer('zoneWelcome').subscribe(() => {
-        currentPopup = WA.ui.openPopup("popupWelcome", "Hi guys, welcome on the BBI travel Hub ! To find your way, consult the map here.", [
+        popup = WA.ui.openPopup("popupWelcome", "Hi guys, welcome on the VSL Travel Hub! To find your way consult the map here.", [
             {
                 className: "primary",
                 label: "Open the map",
-                callback: () => { WA.nav.openCoWebSite("/map.pdf") }
+                callback: async () => {
+                    website = await WA.nav.openCoWebSite("/map.pdf")
+                }
             }
         ]);
     })
@@ -22,11 +25,13 @@ WA.onInit().then(() => {
     WA.room.onLeaveLayer('zoneWelcome').subscribe(closePopup)
 
     WA.room.onEnterLayer('zoneBuilding').subscribe(() => {
-        currentPopup = WA.ui.openPopup("popupBuilding","Hi, here you can enter the VSL private space.", [
+        popup = WA.ui.openPopup("popupBuilding","Hi, here you can enter the VSL private space.", [
             {
                 className: "primary",
                 label: "Open a new tab",
-                callback: () => { WA.nav.openTab("https://vsl.com/private-space/vsl-job/") }
+                callback: async () => {
+                    website = await WA.nav.openTab("https://vsl.com/private-space/vsl-job/")
+                }
             }
         ]);
     })
@@ -40,12 +45,14 @@ WA.onInit().then(() => {
 
 }).catch(e => console.error(e));
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+function closePopup(){   
+    if (popup !== undefined) {
+        popup.close();
+        popup = undefined;
     }
-    WA.nav.closeCoWebSite()
+    if (website !== undefined) {
+        website.close();
+    }
 }
 
 export {};
